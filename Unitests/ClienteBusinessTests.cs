83ea15a7aa8841ab2836bde51592c3ee804a0167
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-27 18:34:45 -0300

    feat: Projeto para utilizar Unitest

---
+++ Unitests/ClienteBusinessTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business;
+using DTOs;
+
+namespace Unitests
+{
+    public class ClienteBusinessTests
+    {
+        [Test]
+        public void CalcularIdade_DadosValidos_RetornaIdade()
+        {
+            //Arrange
+            ClienteBusiness clienteBusiness = new ClienteBusiness();
+            ClienteDTO clienteDTO = new ClienteDTO { Nome = "Nome 1", DataNascimento = new DateTime(2010, 1, 1) };
+            int expectedResult = 13;
+
+            //Act
+            int result = clienteBusiness.CalcularIdade(clienteDTO);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+        }
+    }
+}
